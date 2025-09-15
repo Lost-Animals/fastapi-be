16b6db63682edc8965706aff4081e3b683939ed3
DamyanBG <d.dimitrov.1988@abv.bg>
2025-09-15 19:07:27 +0300

    fix missing env variable

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SA_KEY_PATH = os.environ["SA_KEY_PATH"]
+SA_KEY_PATH = os.environ.get("SA_KEY_PATH")
 BUCKET_NAME = os.environ["BUCKET_NAME"]
 DATABASE_NAME = os.environ["DATABASE_NAME"]
 JWT_KEY = os.environ["JWT_KEY"]
